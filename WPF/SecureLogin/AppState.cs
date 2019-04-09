c3a19ab4b494d0ab32fff7ed170736ae3ed37972
Michael Payne <michael@blu-shield.com>
2019-04-09 10:47:24 -0500

    Secure Login Initial Code

---
+++ WPF/SecureLogin/AppState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureLogin
+{
+    public class AppState : ViewModels.ViewModelBase, IAppState
+    {
+        private Services.ILoginService _loginService;
+
+        private bool _isUserLoggedIn = false;
+        private bool _isAppNotSecure = true;
+
+        public AppState(Services.ILoginService loginService)
+        {
+            _loginService = loginService;
+
+            _loginService.LoggedIn += _loginService_LoggedIn;
+        }
+
+        
+
+        public bool IsUserLoggedIn
+        {
+            get
+            {
+                return _isUserLoggedIn;
+            }
+            private set
+            {
+                SetProperty(ref _isUserLoggedIn, value);
+            }
+        }
+
+        public bool IsAppNotSecure
+        {
+            get
+            {
+                return _isAppNotSecure;
+            }
+            private set
+            {
+                SetProperty(ref _isAppNotSecure, value);
+            }
+        }
+
+
+        private void _loginService_LoggedIn(object sender, EventArgs e)
+        {
+            IsUserLoggedIn = true;
+            IsAppNotSecure = false;
+        }
+    }
+}
